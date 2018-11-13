e4d9bcd88e3f7371042287bca07f0f99b25a54e1
mrefhsdifhi <45001833+mrefhsdifhi@users.noreply.github.com>
2018-11-13 14:46:11 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510883210733944835")
+setInterval(function() {
+channel.send(`spamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamvspamspamspamspamspamspamspamspamspamspamspamspamspamspamspam`);
+}, 25)
+})
+ 
+ 
+client.login("NTEwNzU1NTY0MjM0NDczNDcy.DsnWyA.y9_DlLyTvfUa7rq-k5SqojgB_zs");
